4fe6d6bb41cd1346e6096afedcc97d91c8025db2
david steele <davidsteele@microsoft.com>
2024-09-19 11:43:25 +0100

    Update file copyright header

--- azure_auth.py
+++ azure_auth.py
@@ -1,5 +1,16 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Highly Confidential Material
+# Copyright (C) Microsoft Corporation.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import logging
 import dnf
